d8979e27e30395b96ebc5a44302851a6047d0ab3
Jordan Debord <ZJAD35@ETSU.EDU>
2021-04-28 12:20:29 -0400

    updated css

---
+++ JordanDeBordProject3/Models/ViewModels/AppNameVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JordanDeBordProject3.Models.ViewModels
+{
+    public class AppNameVM
+    {
+        public string AppName { get; set; }
+    }
+}
---
+++ JordanDeBordProject3/ViewComponents/AppNameViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using JordanDeBordProject3.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JordanDeBordProject3.ViewComponents
+{
+    public class AppNameViewComponent : ViewComponent
+    {
+        private readonly IConfiguration _config;
+
+        public AppNameViewComponent(IConfiguration config) 
+        {
+            _config = config;
+        }
+
+        public IViewComponentResult Invoke() 
+        {
+            var appName = _config.GetValue<string>("ApplicationName");
+            var model = new AppNameVM { AppName = appName };
+            return View(model);
+        }
+    }
+}
